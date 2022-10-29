ab231c78302c8244ea63feed0a74900e068b5504
Gonzalo Duarte <megaboxtpro@gmail.com>
2022-10-29 18:57:13 -0300

    Agregando backendportfolio

---
+++ backend/src/main/java/com/portfolio/ProyectoFinal/Security/Dto/LoginUsuario.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.ProyectoFinal.Security.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+
+public class LoginUsuario {
+    @NotBlank
+    private String nombreUsuario;
+    @NotBlank
+    private String passWord;
+    
+    // Getter and Setter
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+    
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,6 +16,9 @@ import { HysComponent } from './components/hys/hys.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import {NgForm} from '@angular/forms';
 
 
 @NgModule({
@@ -31,13 +34,16 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     FooterComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
